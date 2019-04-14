48ddcb731e6e642aef62770402fbe01d481f1047
Daniel Lasry <daniellasry@gmail.com>
2019-04-14 15:37:18 -0700

    Upgraded to python 3

--- release_script/make-release.py
+++ release_script/make-release.py
@@ -8,7 +8,6 @@
 #   https://github.com/OpenBCI/OpenBCI_GUI/releases
 #
 #   Usage: > python release_script/make-release.py
-#   Written for python 2.7, but could easily be adapted to python 3.
 #   No warranty. Use at your own risk. 
 #
 #########################################################################################
@@ -67,21 +66,21 @@ def find_sketch_dir():
 ### Function: Clean up any old build directories or .zips
 ###########################################################
 def cleanup_build_dirs(sketch_dir, zips = False):
-    print "Cleanup ..."
+    print ("Cleanup ...")
     for dir in all_flavors:
         full_dir = os.path.join(sketch_dir, dir)
         full_zip_dir = full_dir + ".zip"
         full_dmg_dir = full_dir + ".dmg"
         if os.path.isdir(full_dir):
             shutil.rmtree(full_dir)
-            print "Successfully deleted " + full_dir
+            print ("Successfully deleted " + full_dir)
         if zips:
             if os.path.isfile(full_zip_dir):
                 os.remove(full_zip_dir)
-                print "Successfully deleted " + full_zip_dir
+                print ("Successfully deleted " + full_zip_dir)
             if os.path.isfile(full_dmg_dir):
                 os.remove(full_dmg_dir)
-                print "Successfully deleted " + full_dmg_dir
+                print ("Successfully deleted " + full_dmg_dir)
 
 ### Function: Ask user for windows signing info
 ###########################################################
@@ -90,13 +89,13 @@ def ask_windows_signing():
     windows_pfx_path = ''
     windows_pfx_password = ''
     if LOCAL_OS == WINDOWS:
-        is_signing = raw_input("Will you be signing the app? (Y/n): ")
+        is_signing = input("Will you be signing the app? (Y/n): ")
         if is_signing.lower() != 'n':
             windows_signing = True
-            windows_pfx_path = raw_input("Path to PFX file: ")
+            windows_pfx_path = input("Path to PFX file: ")
             while not os.path.isfile(windows_pfx_path):
-                windows_pfx_path = raw_input("PFX file not found. Re-enter: ")
-            windows_pfx_password = raw_input("Password for the PFX file: ")
+                windows_pfx_path = input("PFX file not found. Re-enter: ")
+            windows_pfx_password = input("Password for the PFX file: ")
 
     return windows_signing, windows_pfx_path, windows_pfx_password
 
@@ -105,7 +104,7 @@ def ask_windows_signing():
 def build_app(sketch_dir):
     # unfortunately, processing-java always returns exit code 1,
     # so we can't reliably check for success or failure
-    print "Using sketch: " + sketch_dir
+    print ("Using sketch: " + sketch_dir)
     subprocess.call(["processing-java", "--sketch=" + sketch_dir, "--export"])
 
 ### Function: Package the app in the expected file structure
@@ -121,31 +120,31 @@ def package_app(sketch_dir, flavor, windows_signing=False, windows_pfx_path = ''
     try:
         shutil.rmtree(source_dir)
     except OSError as err:
-        print err
-        print "WARNING: Could not delete source dir: " + source_dir
+        print (err)
+        print ("WARNING: Could not delete source dir: " + source_dir)
     else:
-        print "Successfully deleted source dir."
+        print ("Successfully deleted source dir.")
 
     ### Ask user for the hub directory
     ###########################################################
-    hub_dir = raw_input("Drag and drop the HUB for " + flavor + " [ENTER to skip]: ")
+    hub_dir = input("Drag and drop the HUB for " + flavor + " [ENTER to skip]: ")
 
     if hub_dir: # if the hub_dir is not empty (user did not skip)
         if LOCAL_OS == WINDOWS:
             # sanity check: does this directory contain the hub executable?
             hub_exe = os.path.join(hub_dir, "OpenBCIHub.exe")
             while not os.path.isfile(hub_exe):
-                hub_dir = raw_input("OpenBCIHub.exe not found in this directory, please re-enter: ")
+                hub_dir = input("OpenBCIHub.exe not found in this directory, please re-enter: ")
                 hub_exe = os.path.join(hub_dir, "OpenBCIHub.exe")
         if LOCAL_OS == LINUX:
             # sanity check: does this directory contain the hub executable?
             hub_exe = os.path.join(hub_dir, "OpenBCIHub")
             while not os.path.isfile(hub_exe):
-                hub_dir = raw_input("OpenBCIHub executable not found in this directory, please re-enter: ")
+                hub_dir = input("OpenBCIHub executable not found in this directory, please re-enter: ")
                 hub_exe = os.path.join(hub_dir, "OpenBCIHub")
         elif LOCAL_OS == MAC:
             while not hub_dir.endswith("OpenBCIHub.app"):
-                hub_dir = raw_input("Expected a path to OpenBCIHub.app, please re-enter:")
+                hub_dir = input("Expected a path to OpenBCIHub.app, please re-enter:")
 
         ### Copy the Hub to the data directory
         ###########################################################
@@ -159,13 +158,13 @@ def package_app(sketch_dir, flavor, windows_signing=False, windows_pfx_path = ''
         try:
             shutil.copytree(hub_dir, hub_dest_dir, symlinks=True)
         except shutil.Error as err:
-            print err
-            print "WARNING: Failed to copy the Hub to the data dir."
+            print (err)
+            print ("WARNING: Failed to copy the Hub to the data dir.")
         except OSError as err:
-            print err
-            print "WARNING: Failed to copy the Hub to the data dir. Perhaps it already exists?"
+            print (err)
+            print ("WARNING: Failed to copy the Hub to the data dir. Perhaps it already exists?")
         else:
-            print "Successfully copied Hub to the data dir."
+            print ("Successfully copied Hub to the data dir.")
 
     ### On mac, copy the icon file and sign the app
     ###########################################################
@@ -176,19 +175,19 @@ def package_app(sketch_dir, flavor, windows_signing=False, windows_pfx_path = ''
         try:
             shutil.copy2(icon_dir, icon_dest)
         except IOError:
-            print "WARNING: Failed to copy sketch.icns"
+            print ("WARNING: Failed to copy sketch.icns")
         else:
-            print "Successfully copied sketch.icns"
+            print ("Successfully copied sketch.icns")
 
         # sign the app
         try:
             subprocess.check_call(["codesign", "-f", "-v", "-s"\
                 "Developer ID Application: OpenBCI, Inc. (3P82WRGLM8)", app_dir])
         except subprocess.CalledProcessError as err:
-            print err
-            print "WARNING: Failed to sign app."
+            print (err)
+            print ("WARNING: Failed to sign app.")
         else:
-            print "Successfully signed app."
+            print ("Successfully signed app.")
 
     ### On Windows, just sign the app
     ###########################################################
@@ -199,8 +198,8 @@ def package_app(sketch_dir, flavor, windows_signing=False, windows_pfx_path = ''
             subprocess.check_call(["SignTool", "sign", "/f", windows_pfx_path, "/p",\
                 windows_pfx_password, "/tr", "http://tsa.starfieldtech.com", "/td", "SHA256", exe_dir])
         except subprocess.CalledProcessError as err:
-            print err
-            print "WARNING: Failed to sign app."
+            print (err)
+            print ("WARNING: Failed to sign app.")
 
     ### On Mac, make a .dmg and sign it
     ###########################################################
@@ -211,25 +210,25 @@ def package_app(sketch_dir, flavor, windows_signing=False, windows_pfx_path = ''
             subprocess.check_call(["dmgbuild", "-s", "release_script/dmgbuild_settings.py", "-D",\
                 "app=" + app_dir, "OpenBCI_GUI", dmg_dir])
         except subprocess.CalledProcessError as err:
-            print err
-            print "WARNING: Failed create the .dmg file."
+            print (err)
+            print ("WARNING: Failed create the .dmg file.")
         else:
-            print "Successfully created " + dmg_dir
+            print ("Successfully created " + dmg_dir)
 
         # sign the dmg
         try:
             subprocess.check_call(["codesign", "-f", "-v", "-s"\
                 "Developer ID Application: OpenBCI, Inc. (3P82WRGLM8)", dmg_dir])
         except subprocess.CalledProcessError as err:
-            print err
-            print "WARNING: Failed to sign dmg."
+            print (err)
+            print ("WARNING: Failed to sign dmg.")
         else:
-            print "Successfully signed dmg."
+            print ("Successfully signed dmg.")
 
     ### Else zip the file
     ###########################################################
     else:
-        print "Zipping ..."
+        print ("Zipping ...")
         zip_dir = build_dir + ".zip"
 
         # fix the directory structure: application.windows64/OpenBCI_GUI/OpenBCI_GUI.exe
@@ -237,7 +236,7 @@ def package_app(sketch_dir, flavor, windows_signing=False, windows_pfx_path = ''
         os.rename(build_dir, temp_dir)
         os.mkdir(build_dir)
         shutil.move(temp_dir, build_dir)
-        print "Done: " + shutil.make_archive(build_dir, 'zip', build_dir)
+        print ("Done: " + shutil.make_archive(build_dir, 'zip', build_dir))
 
 ### Build Sequence
 ###########################################################
