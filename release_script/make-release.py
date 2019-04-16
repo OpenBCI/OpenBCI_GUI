9bfc6238ec4d3e1ba64660e85525796a3b29e6ea
Daniel Lasry <daniellasry@Daniels-Mini.hsd1.wa.comcast.net>
2019-04-15 22:02:57 -0700

    FIX: release-script on python 2

--- release_script/make-release.py
+++ release_script/make-release.py
@@ -14,6 +14,10 @@
 
 import sys, os, shutil, platform, subprocess
 
+# Fix Python 2.x.
+try: input = raw_input
+except NameError: pass
+
 ### Define platform-specific strings
 ###########################################################
 WINDOWS = 'Windows'
