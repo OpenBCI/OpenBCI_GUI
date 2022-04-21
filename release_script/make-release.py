e59c7a22b7f180a1bc33bb8c057b04fe5da52d70
Eva <52836107+evaesteban@users.noreply.github.com>
2022-04-21 17:43:40 -0400

    debugging print

--- release_script/make-release.py
+++ release_script/make-release.py
@@ -273,6 +273,7 @@ def package_app(sketch_dir, flavor, timestamp, windows_signing=False, windows_pf
         ###########################################################
         if windows_signing:
             try:
+                print("Attempting to sign")
                 subprocess.check_call(["SignTool", "sign", "/f", windows_pfx_path, "/p",\
                     windows_pfx_password, "/tr", "http://timestamp.digicert.com", "/td", "SHA256", exe_dir])
             except subprocess.CalledProcessError as err:
